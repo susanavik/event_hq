176af235ce23d8be0712ce4754153faadb9319db
Susana <susana.m.vik@gmail.com>
2021-04-01 11:26:32 -0400

    added edit/delete button, which kinda work and am starting on add some css

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -21,6 +21,23 @@ class InvitesController < ApplicationController
         redirect_to invite_path(@invite.id)
     end
 
+    def edit
+        @invite = Invite.find(params[:id])
+    end
+
+    def update
+        @invite = Invite.update(invite_params)
+        
+        redirect_to invite_path(@invite)
+    end
+
+    def destroy
+        @invite = Invite.find(params[:id])
+        @invite.destroy
+
+        redirect_to invites_path
+    end
+
     def invite_params
         params.require(:invite).permit(:title, :event_id, :user_id)
     end
